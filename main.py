a9510add1b38c958474e78bcf3f53118f9ba44b3
depuc <koripallipraveendeepak@gmail.com>
2025-07-14 23:09:58 +0530

    Added create_files and create_folder

--- main.py
+++ main.py
@@ -34,19 +34,37 @@ class File_Manager:
         self.move_button=tk.Button(self.buttons_frame, text='Move',command=self.move_item)
         self.move_button.pack(side=tk.LEFT)
 
+
     def update_file_list(self):
         self.file_listbox.delete(0,tk.END)
         self.path_label.config(text=self.current_dir)
         for item in os.listdir(self.current_dir):
             self.file_listbox.insert(tk.END,item)
+
+
     def on_item_double_click(self, event):
-        pass
+        selected_item = self.file_listbox.get(self.file_listbox.curselection())
+        selected_path = os.path.join(self.current_dir, selected_item) 
+        if os.path.isdir(selected_path):
+            self.current_dir = selected_path
+            self.update_file_list()
+
+
     def create_folder(self):
-        pass
+        folder_name = simpledialog.askstring("Create Folder", "Enter folder name: ")
+        if folder_name:
+            os.makedirs(os.path.join(self.current_dir,folder_name),exist_ok =True)
+            self.update_file_list()
+
+
     def create_file(self):
-        pass
+        file_name = simpledialog.askstring("Create File", "Enter File name: ")
+        open(os.path.join(self.current_dir,file_name),'w').close()
+        self.update_file_list()
+
+        
     def delete_item(self):
-        pass
+        pass 
     def copy_item(self):
         pass
     def move_item(self):
